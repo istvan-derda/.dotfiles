577d062294ac22d3df6b57e04689fe4bc4bbe19e
Istvan Derda <istvan.derda@spreadgroup.com>
2022-05-24 14:40:34 +0200

    add poetry

--- install.sh
+++ install.sh
@@ -30,3 +30,6 @@ sudo dnf install gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite \
 sqlite-devel openssl-devel xz xz-devel libffi-devel patch # pyenv dependencies
 
 curl https://pyenv.run | zsh # pyenv installer
+
+# install poetry
+curl -sSL https://install.python-poetry.org | python3 -
