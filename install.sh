48291e8d8302d8acc5cde845463625b87829eb5a
Istvan Derda <istvan.derda@spreadgroup.com>
2022-05-24 13:15:27 +0200

    add fzf

--- install.sh
+++ install.sh
@@ -1,6 +1,25 @@
+# install util-linux-user
+sudo dnf install util-linux-user # provides chsh
+
 # install stow
-dnf install stow
+sudo dnf install stow -y
 
+# install zsh
+sudo dnf install zsh -y
 
-# activate bash config
-stow bash
+# add zsh to valid shells
+command -v zsh | sudo tee -a /etc/shells
+
+# make zsh default shell 
+sudo chsh -s $(which zsh) $USER
+
+# Install antibody and generate zsh_plugins.sh
+curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
+
+antibody bundle < .zsh_plugins.txt > .zsh_plugins.sh
+
+# activate zsh config
+stow zsh
+
+# install additional packages
+dnf install bat tldr
